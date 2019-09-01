4b15c7f05969c13f35850b45b98c94f9bfa9ddb3
unknown <maverick.somu@gmail.com>
2019-09-01 02:39:09 +0000

    Add post , edit , search unit tests

--- CustomerTests/CustomerTests.cs
+++ CustomerTests/CustomerTests.cs
@@ -2,6 +2,7 @@ using CustomerApi.Controllers;
 using CustomerApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -64,22 +65,84 @@ namespace CustomerTests
             // Arrange
             var dbContext = DbContextMocker.GetCustomerContext(nameof(TestGetCustomersAsync));
             var controller = new CustomerController(dbContext);
-            var customerId = 3;
+            var customer = new Customer
+            {
+                Id = 5,
+                FirstName = "Nelson",
+                LastName = "Mandela",
+                DateOfBirth = Convert.ToDateTime("1918-07-18T00: 00:00")
+            };
 
             // Act
-            var response = await controller.GetCustomer(customerId);
-            var customerObj = response.Value;
+
+            var response = await controller.PostCustomer(customer);
+            CreatedAtActionResult result = response.Result as CreatedAtActionResult;
+            var statusCode = result.StatusCode;
+            var customerVal = result.Value as Customer;
 
             //dispose dbcntext
             dbContext.Dispose();
 
             // Assert
-            Assert.NotNull(customerObj);
-            Assert.Same(customerObj.FirstName, "John");
-            Assert.Same(customerObj.LastName, "Doe");
-
+            Assert.NotNull(result);
+            Assert.NotNull(statusCode);
+            Assert.Equal(201, statusCode);
+            Assert.Same(customerVal.FirstName, "Nelson");
+            Assert.Same(customerVal.LastName, "Mandela");
+            Assert.Equal(customerVal.DateOfBirth, Convert.ToDateTime("7/18/1918 12:00:00 AM"));
         }
 
+        [Fact]
+        public async Task TestEditCustomerAsync()
+        {
+
+            // Arrange
+            var dbContext = DbContextMocker.GetCustomerContext(nameof(TestGetCustomersAsync));
+            var controller = new CustomerController(dbContext);
+            var customerId = 4;
+
+            // Act
+
+            var response = await controller.GetCustomer(customerId);
+            var customerObj = response.Value as Customer;
+
+            //edit customer values
+            customerObj.LastName = "Smith";
+            customerObj.DateOfBirth = Convert.ToDateTime("1986-11-21T00: 00:00");
+
+            var editResponse = await controller.PutCustomer(customerId, customerObj) as NoContentResult; 
+
+            //dispose dbcntext
+            dbContext.Dispose();
+
+            // Assert
+            Assert.NotNull(editResponse);
+            Assert.Equal(204, editResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task TestSearchCustomerAsync()
+        {
+
+            // Arrange
+            var dbContext = DbContextMocker.GetCustomerContext(nameof(TestGetCustomersAsync));
+            var controller = new CustomerController(dbContext);
+            var customerId = 4;
+
+            // Act
+
+            var response = await controller.FindCustomers("Jane");
+            var customers = response.Value as List<Customer>;
+            
+
+            //dispose dbcntext
+            dbContext.Dispose();
+
+            // Assert
+            Assert.NotNull(customers);
+            var customerJane = customers[0];
+            Assert.Same(customerJane.FirstName, "Jane");
+        }
 
     }
 
